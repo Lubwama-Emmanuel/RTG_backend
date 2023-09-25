285b409fdc48ce40f22c5d0e5e7e910fe9ab0bd2
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-25 18:10:38 +0300

    feat:(added image upload controller)

--- controllers/laptopController.js
+++ controllers/laptopController.js
@@ -1,46 +1,37 @@
 const Laptop = require("../models/laptopModel");
 const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
 const path = require("path");
 
 // const storage = multer.memoryStorage();
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, path.join("./public/uploads"));
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-
-const storage = new GridFsStorage({
-  url,
-  file: (req, file) => {
-    // Check if its an image before saving to photos bucket
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      return {
-        bucketName: "images",
-        filename: `${Date.now()}_${file.originalname}`,
-      };
-    } else {
-      // otherwise save to the default bucket
-      return `${Date.now()}_${file.originalname}`;
-    }
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join("./public/uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   },
 });
 
-exports.upload = multer({ storage });
+const fileFilter = function (req, file, cb) {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+exports.upload = multer({ storage, fileFilter });
 
 exports.addLaptop = async (req, res) => {
   try {
-    const imageBuffer = req.file;
     const { name } = req.body;
+    const image = req.file.path;
 
-    // const newLaptop = await Laptop.create({ image: imageBuffer, name: name });
+    const newLaptop = await Laptop.create({ name, image });
 
     res.status(201).json({
       status: "success",
-      // data: newLaptop,
+      data: newLaptop,
     });
   } catch (error) {
     console.error(error);
