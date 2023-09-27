2e0d9437c038fcfc856dd0f6e224be7419bcb15d
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-27 12:43:55 +0300

    feat:(added more data objs to the model)

--- models/laptopModel.js
+++ models/laptopModel.js
@@ -7,6 +7,32 @@ const laptopSchema = mongoose.Schema({
   image: {
     type: String,
   },
+  brand: {
+    type: String,
+  },
+  productLine: {
+    type: String,
+  },
+  model: {
+    type: String,
+  },
+  processor: {
+    type: String,
+    enum: ["intel", "amd"],
+  },
+  core: {
+    type: String,
+  },
+  generation: {
+    type: String,
+  },
+  storage: {
+    type: String,
+    enum: ["ssd", "hdd"],
+  },
+  size: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("Laptop", laptopSchema);
