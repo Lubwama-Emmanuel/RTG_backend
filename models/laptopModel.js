bfc509421bb7beadbbd58ae89dd4f6cc6dac52d9
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-16 16:49:43 +0300

    refactor:refactored to postgres

--- models/laptopModel.js
+++ models/laptopModel.js
@@ -1,48 +1,48 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const laptopSchema = mongoose.Schema({
-  name: {
-    type: String,
-  },
-  mainImage: {
-    type: String,
-  },
-  brand: {
-    type: String,
-  },
-  productLine: {
-    type: String,
-  },
-  model: {
-    type: String,
-  },
-  processor: {
-    type: String,
-  },
-  core: {
-    type: String,
-  },
-  generation: {
-    type: String,
-  },
-  storage: {
-    type: String,
-  },
-  size: {
-    type: String,
-  },
-  otherImages: {
-    type: Array,
-  },
-  desc: {
-    type: String,
-  },
-  screen: {
-    type: String,
-  },
-  price: {
-    type: Number,
-  },
-});
+// const laptopSchema = mongoose.Schema({
+//   name: {
+//     type: String,
+//   },
+//   mainImage: {
+//     type: String,
+//   },
+//   brand: {
+//     type: String,
+//   },
+//   productLine: {
+//     type: String,
+//   },
+//   model: {
+//     type: String,
+//   },
+//   processor: {
+//     type: String,
+//   },
+//   core: {
+//     type: String,
+//   },
+//   generation: {
+//     type: String,
+//   },
+//   storage: {
+//     type: String,
+//   },
+//   size: {
+//     type: String,
+//   },
+//   otherImages: {
+//     type: Array,
+//   },
+//   desc: {
+//     type: String,
+//   },
+//   screen: {
+//     type: String,
+//   },
+//   price: {
+//     type: Number,
+//   },
+// });
 
-module.exports = mongoose.model("Laptop", laptopSchema);
+// module.exports = mongoose.model("Laptop", laptopSchema);
--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 const app = require("./app");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 3003;
-
-const DB = process.env.MONGODB_URL.replace(
-  "<PASSWORD>",
-  process.env.MONGODB_PASSWORD
-);
-
-// CONNECT TO DATABASE
-mongoose
-  .connect(DB)
-  .then(() => console.log(`---DATABASE CONNECTED SUCCESSFULLY`))
-  .catch((error) =>
-    console.log(`Ooooooops DATABASE NOT CONNECTED ERROR: ${error}`)
-  );
 
 app.listen(port, () => {
   console.log(`----server tappin on port:${port}`);
