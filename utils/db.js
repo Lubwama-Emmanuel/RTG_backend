a0d49c5b87d3c788a3d0b5b72703970e6c3094a4
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-16 16:48:23 +0300

    feat:changed db to postgres

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
+  user: process.env.USER,
   host: "localhost",
-  database: "rtg",
-  password: "6723",
-  port: 5432,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
 });
 
 module.exports = pool;
